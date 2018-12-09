cb2d19c57b7cc36067babd5888592ee6903c6924
David Nicolau <ulin666@gmail.com>
2018-12-09 21:00:50 +0100

    Tested method allows new lines as delimiter

--- dnicolau_tdd_kata.tests/StringCalculatorTest.cs
+++ dnicolau_tdd_kata.tests/StringCalculatorTest.cs
@@ -17,7 +17,10 @@ namespace dnicolau_tdd_kata.tests
         [DataTestMethod]
         [DataRow("", 0)]
         [DataRow("1", 1)]
-        [DataRow("1,2", 3)]        
+        [DataRow("1,2", 3)]
+        [DataRow("1,4,5,2", 12)] //Unkown number of numbers
+        [DataRow("10,4,5,2", 21)] //Unkown number of numbers
+        [DataRow("1\n2,3", 6)] //Allow new lines between numbers as separator
         public void add_numbers_numbersum(string numbers, int expected)
         {
             int actual = stringCalculator.add(numbers);
