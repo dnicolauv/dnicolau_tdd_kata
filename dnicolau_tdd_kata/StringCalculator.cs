d7d78f971b3b6c277ac710083034f6c40dc7c83a
David Nicolau <ulin666@gmail.com>
2018-12-09 20:43:39 +0100

    Add method refactored to handle all three use cases (empty, one number and two numbers) withouth repetition

--- dnicolau_tdd_kata/StringCalculator.cs
+++ dnicolau_tdd_kata/StringCalculator.cs
@@ -7,22 +7,14 @@ namespace dnicolau_tdd_kata.tests
         public int add(string numbers)
         {
             int result = 0;
-
-            if (String.IsNullOrEmpty(numbers))
-                result =  0;
-
             int number = 0;
-            if (Int32.TryParse(numbers, out number))
-                result = number;
-            else
-            {
-                var numArray = String.Join(",", numbers);
 
-                foreach (char num in numArray)
-                {
-                    if (Int32.TryParse(num.ToString(), out number))
-                        result += number;
-                }
+            var numArray = String.Join(",", numbers);
+
+            foreach (char num in numArray)
+            {
+                if (Int32.TryParse(num.ToString(), out number))
+                    result += number;
             }
 
             return result;
