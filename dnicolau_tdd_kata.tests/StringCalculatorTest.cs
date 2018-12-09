888087922ff2522554bdc22454e89083101cd726
David Nicolau <ulin666@gmail.com>
2018-12-09 20:28:47 +0100

    First test for method add when param is empty

--- dnicolau_tdd_kata.tests/StringCalculatorTest.cs
+++ dnicolau_tdd_kata.tests/StringCalculatorTest.cs
@@ -6,8 +6,13 @@ namespace dnicolau_tdd_kata.tests
     public class StringCalculatorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void add_empty_zero()
         {
+            StringCalculator sut = new StringCalculator();
+            string empty = string.Empty;
+            int actual = sut.add(empty);
+            int expected = 0;
+            Assert.AreEqual(actual, expected);
         }
     }
 }
---
+++ dnicolau_tdd_kata/StringCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dnicolau_tdd_kata.tests
+{
+    public class StringCalculator
+    {
+        public int add(string numbers)
+        {
+            int result = 0;
+
+            if (String.IsNullOrEmpty(numbers))
+                result =  0;
+
+            return result;
+        }
+    }
+}
