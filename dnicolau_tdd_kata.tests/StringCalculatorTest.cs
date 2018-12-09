17fd676f11a0f4ca1e342ab592462e942927884e
David Nicolau <ulin666@gmail.com>
2018-12-09 19:21:08 +0100

    Initial commit

---
+++ dnicolau_tdd_kata.tests/StringCalculatorTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dnicolau_tdd_kata.tests
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
